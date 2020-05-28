523d694eeead719cf67af126bb9ad75b4d497b96
Jonathan Heinz <jheinzdev@gmail.com>
2020-05-28 13:14:10 -0700

    added cors

--- api/app.ts
+++ api/app.ts
@@ -1,4 +1,7 @@
-import { use } from 'nexus'
+import { use, server } from 'nexus'
 import { prisma } from 'nexus-plugin-prisma'
+import cors from 'cors';
 
 use(prisma())
+
+server.express.use(cors());
