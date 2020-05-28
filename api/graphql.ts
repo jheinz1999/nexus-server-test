4bb6ed34ddb098d4944c8cc736bc07e74a867f80
Jonathan Heinz <jheinzdev@gmail.com>
2020-05-28 10:46:13 -0700

    can query users

--- api/graphql.ts
+++ api/graphql.ts
@@ -1,2 +1,24 @@
 import { schema } from "nexus"
 
+schema.objectType({
+  name: 'users',
+  definition(t) {
+    t.model.id();
+    t.model.email_address();
+    t.model.first_name();
+    t.model.last_name();
+    t.model.phone_country_code();
+    t.model.phone_national_number();
+  },
+});
+
+schema.queryType({
+  definition(t) {
+    t.list.field('users', {
+      type: 'users',
+      resolve(_root, _args, ctx) {
+        return ctx.db.users.findMany();
+      }
+    })
+  }
+});
